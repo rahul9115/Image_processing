374adf4549ad746de9413d4ac3b6929ba3d83c1d
rahul9115 <46648019+rahul9115@users.noreply.github.com>
2021-04-29 00:29:07 +0530

    Completed with more images

--- check2.py
+++ check2.py
@@ -8,7 +8,7 @@ def digit_recognition():
     k=0
     for m in range(1,7,1):
         img=cv.imread(f"{m}.png")
-        img=cv.resize(img,(300,300),cv.INTER_AREA)
+        img=cv.resize(img,(120,120),cv.INTER_AREA)
         cv.imshow("image",img)
         output=reader.readtext(img,allowlist="0123456789")
         k=0
@@ -20,17 +20,20 @@ def digit_recognition():
                     if(len(j)>1):
                         val=list(j)
                         final_digits.append(int(val[0]))
-                    else:
+                    elif (j!=''):
                         final_digits.append(int(j))
                         print(j)
                  
 def digit_extraction():
-    x=28
-    w=25
+    x=5
+    w=28
     #y=9
     #h=25
-    y=0
-    h=35
+    y=5
+    h=40
+    #Capture image 6 values x=5 w=28 y=5 h=40
+    #Capture image 6 values x=5 w=45 y=5 h=50
+    #Capture image 5 values x=5 w=45 y=0 h=50
     #Capture image 4 values x=28 w=25 y=0 h=35
     #Capture image 3 values x=0 w=27 y=0 h=40
     #Capture image 2 values x=0 w=20 y=9 h=25
@@ -46,12 +49,14 @@ def digit_extraction():
     digit_recognition()  
 
 def image_crop():
-    img=cv.imread("Capture4.PNG")
-    img=img[70:110,70:280]
+    img=cv.imread("Capture6.PNG")
+    img=img[52:78,30:200]
     plt.imshow(img)
     plt.show()
     cv.waitKey(0)
-    #img=img[70:110,80:280]
+    #img=img[90:140,56:343]Capture5
+    #img=img[70:110,70:280]Capture 4
+    #img=img[70:110,80:280]Capture3
     #img=img[70:90,80:180]Capture2
     #plt.imshow(img[90:130,25:175]) meter.jpg
     #img=img[90:130,25:175]
--- lcd.py
+++ lcd.py
@@ -1,14 +1,14 @@
 import cv2 as cv
 import matplotlib.pyplot as plt
-img=cv.imread("Capture4.PNG")
+img=cv.imread("Capture6.PNG")
 plt.imshow(img)
-img=img[70:110,70:280]
-x=28
-w=25
+img=img[52:78,30:200]
+x=5
+w=26
 #y=9
 #h=25
-y=0
-h=35
+y=5
+h=40
 for i in range(1,7,1):
         
         roi = img[y:y + h, x:x + w]
